4bd03420864b70f8137f4989ae56af33156501cc
marekk1992 <m.komaisko@gmail.com>
2022-03-01 22:52:20 +0200

    added updated playlist program

--- src/exercises/playlist/Menu.java
+++ src/exercises/playlist/Menu.java
@@ -35,7 +35,7 @@ public class Menu {
                 case 5:
                     openMusicPlayer();
                     break;
-                case 11:
+                case 6:
                     quit = true;
                     break;
             }
@@ -50,12 +50,16 @@ public class Menu {
         System.out.println("\t 3 - Add song to album.");
         System.out.println("\t 4 - Add song to playlist.");
         System.out.println("\t 5 - Open music player.");
-        System.out.println("\t 6 - Close music player (*only with opened music player).");
-        System.out.println("\t 7 - Replay current song (*only with opened music player).");
-        System.out.println("\t 8 - Play next song (*only with opened music player).");
-        System.out.println("\t 9 - Play previous song (*only with opened music player).");
-        System.out.println("\t 10 - Remove current song from playlist (*only with opened music player).");
-        System.out.println("\t 11 - To quit the application.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+
+    public void printPlayerMenu() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - Close music player.");
+        System.out.println("\t 1 - Replay current song.");
+        System.out.println("\t 2 - Play next song.");
+        System.out.println("\t 3 - Play previous song.");
+        System.out.println("\t 4 - Remove current song from playlist.");
     }
 
     public void createNewAlbum() {
@@ -120,6 +124,7 @@ public class Menu {
             return;
         } else {
             System.out.println("Now playing " + listIterator.next().getTitle());
+            printPlayerMenu();
         }
 
         while (!quit) {
@@ -127,52 +132,73 @@ public class Menu {
             int action = scanner.nextInt();
             scanner.nextLine();
             switch (action) {
-                case 6:
-                    System.out.println("Closing music player...............");
+                case 0:
+                    closeMusicPlayer();
                     quit = true;
                     break;
-                case 7:
-                    if (goingForward) {
-                        System.out.println("Now playing " + listIterator.previous().getTitle() + ".");
-                    } else {
-                        System.out.println("Now playing " + listIterator.next().getTitle() + ".");
-                    }
+                case 1:
+                    replaySong(listIterator);
                     break;
-                case 8:
-                    if (!goingForward) {
-                        if (listIterator.hasNext()) {
-                            listIterator.next();
-                        }
-                        goingForward = true;
-                    }
-                    if (listIterator.hasNext()) {
-                        System.out.println("Now playing " + listIterator.next().getTitle());
-                    } else {
-                        System.out.println("Reached the end of playlist.");
-                        goingForward = false;
-                    }
+                case 2:
+                    goingForward = playNextSong(listIterator, goingForward);
                     break;
-                case 9:
-                    if (goingForward) {
-                        if (listIterator.hasPrevious()) {
-                            listIterator.previous();
-                            goingForward = false;
-                        }
-                    }
-                    if (listIterator.hasPrevious()) {
-                        System.out.println("Now playing " + listIterator.previous().getTitle());
-                    } else {
-                        System.out.println("We are at the start of the playlist");
-                        goingForward = true;
-                    }
+                case 3:
+                    goingForward = playPreviousSong(listIterator, goingForward);
                     break;
-                case 10:
-                    int indexToRemove = listIterator.nextIndex();
-                    Song song = database.getPlaylist().get(indexToRemove);
-                    String name = song.getTitle();
-                    System.out.println("Song " + name + " removed from the playlist.");
-                    database.getPlaylist().remove(indexToRemove);
+                case 4:
+                    removeCurrentSong(listIterator);
+                    break;
             }
         }
+    }
+
+    private void closeMusicPlayer() {
+        System.out.println("Closing music player...............");
+        printMenu();
+    }
+
+    private void replaySong(ListIterator<Song> listIterator) {
+        listIterator.next();
+        System.out.println("Now playing " + listIterator.previous().getTitle());
+    }
+
+    private boolean playNextSong(ListIterator<Song> listIterator, boolean movingForwardInList) {
+        if (!movingForwardInList) {
+            if (listIterator.hasNext()) {
+                listIterator.next();
+            }
+            movingForwardInList = true;
+        }
+        if (listIterator.hasNext()) {
+            System.out.println("Now playing " + listIterator.next().getTitle());
+        } else {
+            System.out.println("Reached the end of playlist.");
+            movingForwardInList = false;
+        }
+        return movingForwardInList;
+    }
+
+    private boolean playPreviousSong(ListIterator<Song> listIterator, boolean movingForwardInList) {
+        if (movingForwardInList) {
+            if (listIterator.hasPrevious()) {
+                listIterator.previous();
+                movingForwardInList = false;
+            }
+        }
+        if (listIterator.hasPrevious()) {
+            System.out.println("Now playing " + listIterator.previous().getTitle());
+        } else {
+            System.out.println("We are at the start of the playlist");
+            movingForwardInList = true;
+        }
+        return movingForwardInList;
+    }
+
+    private void removeCurrentSong(ListIterator<Song> listIterator) {
+        int indexToRemove = listIterator.nextIndex();
+        Song song = database.getPlaylist().get(indexToRemove);
+        String name = song.getTitle();
+        System.out.println("Song " + name + " removed from the playlist.");
+        database.getPlaylist().remove(indexToRemove);
     }
 }
